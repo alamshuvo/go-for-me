c19f8fe421e9425f35736d85c2ba77e14caf6a90
alamshuvo <alamshuvozeny019@gmail.com>
2025-09-21 22:06:42 +0600

    some code added

--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
+// go er each file e ekta kore package takhbe 
+// main go file er package hote hobe main 
+// fmt holo formet package eta holo go er built in package 
+// jkn kono variable declare kori tokn sei variable ta ram e store hoyea takhe 
+// Data Type 1 Numeric 2 Boolean 3 String
+// Interger Number      Floating Point Number 
+// 10 						10.05 float32 float64
+// int int8 int16 int32 int64
+// uint uint8 uint16 uint32 uint64 uintptr
 import "fmt"
 
 func main(){
- a := 15
- b := 10
-  fmt.Println(a+b)
+	// // var age int = 22
+	// age :=18
+	// // var age =10
+    // if age>18 {
+	// 	fmt.Println("you are eligble to be married")
+	// } else if age<18 {
+	// 	fmt.Println("You are not eligible to be marriend, but you can love someone ")
+	// }else if age == 18{
+	// 	fmt.Println("You are just a teenager,not eligble to be married")
+	// }
+	age := 20
+	sex:="male"
+	if age>60 || sex =="male" {
+		fmt.Println("You are ready to be married!")
+	}
 }
